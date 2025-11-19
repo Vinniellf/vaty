eff305ec8c324d5af7ce3beba851aedf1b6bed75
Vinniellf <125324872+Vinniellf@users.noreply.github.com>
2025-11-19 15:54:33 -0500

    Add template for competitive programming in C++

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define pb push_back
+#define fill(x,v) memset(x,v,sizeof(x))
+#define all(v) (v).begin(),(v).end()
+#define PI 3.14159265359
+#define int long long
+
+typedef long double ld;
+
+const int MOD = 998244353LL;
+const int INF = 1e9;
+
+
+void solve() {
+}
+
+signed main(){
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
